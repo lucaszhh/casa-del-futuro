17f6bd6881b5089eebc853abfdf771d4959536db
lucaszhh <lucaszarandon24@gmail.com>
2023-03-05 23:49:51 -0300

    config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,11 @@ module.exports = {
 		"react",
 		"@typescript-eslint"
 	],
+	"extends": "next",
+  		"rules": {
+		"react/no-unescaped-entities": "off",
+		"@next/next/no-page-custom-font": "off"
+	},
 	"rules": {
 		"indent": [
 			"error",
