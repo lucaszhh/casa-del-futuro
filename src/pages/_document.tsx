f40f39bd1c3784b4743047821b94097b3c6d7350
lucaszhh <lucaszarandon24@gmail.com>
2024-01-19 19:02:01 -0300

    add title and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<link rel="icon" href="/favicon.jpg.ico" />
+				<link rel="icon" href="/favicon.ico" />
 				<link rel="stylesheet"
 					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
 					integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
@@ -22,7 +22,7 @@ export default function Document() {
 				<meta property="og:image" content="https://casa-del-futuro.vercel.app/imgs/casita.png" />
 				<meta property="og:url" content="https://casa-del-futuro.vercel.app/" />
 				<meta property="og:type" content="website" />
-
+				<title>Casa del futuro</title>
 				<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
 			</Head>
 			<body>
